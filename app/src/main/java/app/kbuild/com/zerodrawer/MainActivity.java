5793854fdc1ca0068bda435ef968f6cd339b7422
Adebayo Osunbiyi <repo@itandtsolutions.net>
2018-01-31 15:43:37 +0100

    Header Customization

--- app/src/main/java/app/kbuild/com/zerodrawer/MainActivity.java
+++ app/src/main/java/app/kbuild/com/zerodrawer/MainActivity.java
@@ -2,12 +2,19 @@ package app.kbuild.com.zerodrawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
+
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
